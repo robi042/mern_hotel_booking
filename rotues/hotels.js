eb69b70145e033e3d15ca1dbe4009c68aedf5cc3
Rabiul Hasan <34013030+robi042@users.noreply.github.com>
2022-09-18 22:31:29 +0600

    room model

--- rotues/hotels.js
+++ rotues/hotels.js
@@ -1,14 +1,15 @@
 import Express from "express"
 
 import {createHotel, updateHotel, deleteHotel, seeHotel, seeHotels} from "../controller/hotel.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = Express.Router();
 
-router.post('/', createHotel)
+router.post('/', verifyAdmin, createHotel)
 
-router.put('/:id', updateHotel)
+router.put('/:id', verifyAdmin, updateHotel)
 
-router.delete('/:id', deleteHotel)
+router.delete('/:id', verifyAdmin, deleteHotel)
 
 
 router.get('/:id', seeHotel)
--- rotues/users.js
+++ rotues/users.js
@@ -4,25 +4,25 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = Express.Router();
 
-router.get('/cheakAuth', verifyToken, (req, res) =>{
-      res.send('you are logged in')
-})
+// router.get('/cheakAuth', verifyToken, (req, res) =>{
+//       res.send('you are logged in')
+// })
 
-router.get('/cheakUser', verifyUser, (req, res) =>{
-      res.send('you are logged in and you can delete your account')
-})
+// router.get('/cheakUser', verifyUser, (req, res) =>{
+//       res.send('you are logged in and you can delete your account')
+// })
 
-router.get('/cheakAdmin', verifyAdmin, (req, res) =>{
-      res.send('you are logged in and you can delete all the account')
-})
+// router.get('/cheakAdmin', verifyAdmin, (req, res) =>{
+//       res.send('you are logged in and you can delete all the account')
+// })
 
-router.put('/:id', updateUser)
+router.put('/:id', verifyUser, updateUser)
 
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyUser, deleteUser)
 
 
-router.get('/:id', seeUser)
+router.get('/:id', verifyUser,seeUser)
 
-router.get('/', seeUsers)
+router.get('/', verifyAdmin, seeUsers)
  
 export default router
